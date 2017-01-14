3a6a63164c79244e856d1911013b961cda288025
warrior5715 <kataihoo@gmail.com>
2017-01-14 15:49:30 -0600

    assignment 1

---
+++ TumblrFeed/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  TumblrFeed
+//
+//  Created by Ka Tai Ho on 1/14/17.
+//  Copyright © 2017 SDLtest. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var moiveImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
